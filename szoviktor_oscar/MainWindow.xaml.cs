7bba7849c41ccdb872236e159a6a3b7ed523d827
Avx <93741404+Golf1ABF@users.noreply.github.com>
2024-11-07 11:21:13 +0100

    Add project files.

---
+++ szoviktor_oscar/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace szoviktor_oscar
+{
+
+    public partial class MainWindow : Window
+    {
+        List<Oscar> lista = new List<Oscar>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var sr = new StreamReader("../../../src/oscar.csv", System.Text.Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Oscar(sr.ReadLine()));
+            }
+
+            foreach (var item in lista.OrderByDescending(item => item.Ev))
+            {
+                lbxFilmek.Items.Add($"{item.Ev}, {item.FilmCime}");
+            }
+
+        }
+
+        private void BtnUjFilm_Click(object sender, RoutedEventArgs e)
+        {
+            var sw = new StreamWriter("../../../src/oscar.csv", true);
+            if (UjCim.Text != null && UjDij.Text != null && UjEv.Text != null && UjJeloles.Text != null)
+            {
+                sw.WriteLine($"{UjCim.Text} \t {UjEv.Text} \t {UjJeloles} \t {UjDij}");
+                MessageBox.Show("Sikeres felvitel!");
+            }
+            else
+            {
+                MessageBox.Show("Hibás adatfelvitel!");
+            }
+        }
+    }
+}
---
+++ szoviktor_oscar/Oscar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoviktor_oscar
+{
+    internal class Oscar
+    {
+        public string FilmAzonosito { get; set; }
+        public string FilmCime { get; set; }
+        public int Ev { get; set; }
+        public int Dij { get; set; }
+        public int Jelol { get; set; }
+
+        public Oscar(string sor) 
+        {
+            var s = sor.Split("\t");
+            this.FilmAzonosito = s[0];
+            this.FilmCime = s[1];
+            this.Ev = int.Parse(s[2]);
+            this.Dij = int.Parse(s[3]);
+            this.Jelol = int.Parse(s[4]);
+        }
+    }
+}
